af196912f93259643ae3b4c040042ec7a37ae3e0
krig6 <ej.ldsm@gmail.com>
2024-05-24 00:45:05 +0800

    feat: Add event listener for close button
    This commit will close the modal container when clicked and support the implementation of CSS animations.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('body');
 const modalContainer = document.querySelector('.modal-container');
 const joinButton = document.querySelector('.join-button');
+const closeButton = document.querySelector('.close-button');
 
 joinButton.addEventListener('click', () => {
     modalContainer.classList.add('is-open');
     body.style.overflow = "hidden";
 });
+
+closeButton.addEventListener('click', () => {
+    modalContainer.classList.remove('is-open');
+    body.style.overflow = "initial";
+})
