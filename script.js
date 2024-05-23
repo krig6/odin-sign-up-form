92d97eef226630b78071d0b33ec4ada824a8e1ee
krig6 <ej.ldsm@gmail.com>
2024-05-24 00:56:40 +0800

    feat: Add event listener for Escape button
    This commit will enable closi
    ng of modal container when Escape key is pressed.
    
    Also, removed body.style.overflow to enable scrolling while modal container is
     open.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const body = document.querySelector('body');
 const modalContainer = document.querySelector('.modal-container');
 const joinButton = document.querySelector('.join-button');
 const closeButton = document.querySelector('.close-button');
 
 joinButton.addEventListener('click', () => {
     modalContainer.classList.add('is-open');
-    body.style.overflow = "hidden";
 });
 
 closeButton.addEventListener('click', () => {
     modalContainer.classList.remove('is-open');
-    body.style.overflow = "initial";
-})
+});
+
+document.addEventListener('keydown', e => {
+    e = e || window.e;
+    e.key === 'Escape' ? modalContainer.classList.remove('is-open') : false;
+});
