2b1bc56242a324a2349884043a85e6ee7d9f28a2
krig6 <ej.ldsm@gmail.com>
2024-05-24 22:20:34 +0800

    feat: Implement first name validation
    This commit will prevent users from entering numbers or blank spaces in first name before moving to other input boxes.
    
    Automatically capitalize the first letter of the name.
    
    Border-color change: green if input is valid or red if invalid.
    
    Includes message/reminder as well to inform user if input is invalid/valid.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const modalContainer = document.querySelector('.modal-container');
 const joinButton = document.querySelector('.join-button');
 const closeButton = document.querySelector('.close-button');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const nameValidation = /^[a-zA-Z\s]+$/;
 
 joinButton.addEventListener('click', () => {
     modalContainer.classList.add('is-open');
@@ -14,3 +17,33 @@ document.addEventListener('keydown', e => {
     e = e || window.e;
     e.key === 'Escape' ? modalContainer.classList.remove('is-open') : false;
 });
+
+function validateFirstName() {
+    const formattedFirstName = firstName.value
+        .replace(/\s+/g, ' ') // Replace consecutive spaces with a single space
+        .toLowerCase() // Convert all letters to lowercase
+        .replace(/(^|\s)\S/g, function (firstLetter) {
+            return firstLetter.toUpperCase(); // Capitalize first letter of each word
+        });
+
+    firstName.value = formattedFirstName;
+
+    if (firstName.value.trim() === '') {
+        firstName.setCustomValidity('Please enter your first name.');
+        firstName.style.border = '2px solid red';
+    } else if (!nameValidation.test(firstName.value)) {
+        firstName.setCustomValidity('Numbers are not allowed in this field.');
+        firstName.style.border = '2px solid red';
+    } else {
+        firstName.setCustomValidity('');
+        firstName.style.border = '2px solid green';
+    }
+}
+
+lastName.addEventListener('focus', () => {
+    validateFirstName();
+    firstName.reportValidity();
+});
+
+firstName.addEventListener('input', validateFirstName);
+
