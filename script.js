df67dbc1d0841822325b2f3320ffb369952c96b1
krig6 <ej.ldsm@gmail.com>
2024-05-24 22:27:51 +0800

    feat: Implement last name validation
    This commit will prevent users from entering numbers or blank spaces in last name before moving to the next input box.
    
    Automatically capitalize the first letter of the last name.
    
    Border-color change: green if input is valid or red if invalid.
    
    Includes message/reminder to inform user if input is valid/invalid.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const joinButton = document.querySelector('.join-button');
 const closeButton = document.querySelector('.close-button');
 const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
 const nameValidation = /^[a-zA-Z\s]+$/;
 
 joinButton.addEventListener('click', () => {
@@ -46,4 +47,32 @@ lastName.addEventListener('focus', () => {
 });
 
 firstName.addEventListener('input', validateFirstName);
+
+function validateLastName() {
+    const formattedLastName = lastName.value
+        .replace(/\s+/g, ' ') // Replace consecutive spaces with a single space
+        .toLowerCase() // Convert all letters to lowercase
+        .replace(/(^|\s)\S/g, function (firstLetter) {
+            return firstLetter.toUpperCase(); // Capitalize first letter of each word
+        });
+
+    lastName.value = formattedLastName;
+    if (lastName.value.trim() === '') {
+        lastName.setCustomValidity('Please enter your last name.');
+        lastName.style.border = '2px solid red';
+    } else if (!nameValidation.test(lastName.value)) {
+        lastName.setCustomValidity('Numbers are not allowed in this field.');
+        lastName.style.border = '2px solid red';
+    } else {
+        lastName.setCustomValidity('');
+        lastName.style.border = '2px solid green';
+    }
+}
+
+email.addEventListener('focus', () => {
+    validateLastName();
+    lastName.reportValidity();
+})
+
+lastName.addEventListener('input', validateLastName);
 
